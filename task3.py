6fee6ffbb3e285dd1277d7b099e99fce31c0a073
Cricton <devicejoy@gmail.com>
2022-11-03 18:12:23 +0300

    laba2

--- task3.py
+++ task3.py
@@ -1,12 +1,35 @@
-a = [[8, 2, 7], [4, 5, 6], [7, 2, 4]]
-for i in len(a):
-    sor = 0
-    for j in len(a[i]):
-        if a[i][j] <= a[i][j+1]:
-            sor = 1
-            break
-    if sor == 0:
-        b = a[i]
+import random
 
+while True:
+    a = []
+    b = row = column = 0
+    try:
+        column = int(input("Введите количество столбцов: "))
+    except ValueError as e:
+        print(e)
 
-print(b)
+    try:
+        row = int(input("Введите количество строк: "))
+    except ValueError as e:
+        print(e)
+
+    for i in range(row):
+            am = []
+            for j in range(column):
+                am.append(random.randint(0, 100))
+            a.append(am)
+    print("\nИсходная матрица:")
+    for i in range(len(a)):
+        print(a[i])
+    for i in range(len(a)):
+        sor = 0
+        for j in range(len(a[i])-1):
+            if a[i][j] >= a[i][j+1]:
+                sor = 1
+                break
+        if sor == 0:
+            a[i].reverse()
+
+    print("\nИтоговая матрица:")
+    for i in range(len(a)):
+        print(a[i])
