cf992068f33c8de65afa194ecc3cbed123ea8979
crictonn <devicejoy@gmail.com>
2022-11-03 19:27:45 +0300

    Финальное четвертое

---
+++ task4.py
@@ -0,0 +1,12 @@
+a = input("Введите первую переменную: ")
+b = input("Введите вторую переменную: ")
+try:
+    a = int(a)
+    b = int(b)
+    print("a / b = ", a/b)
+except TypeError as e:
+    print(e)
+except ZeroDivisionError as z:
+    print(z)
+except ValueError as v:
+    print(v)
